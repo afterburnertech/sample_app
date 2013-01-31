9a4d8418e6a924f3566b57d7dd1131230d398330
afterburnertech <brandon@afterburnertechnologies.com>
2013-01-31 18:21:50 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '02c124d0a651f06d4a9c5d2fdec1e2b0d3c53311021c32221f63b783bb6e57fc73f392b04c6cf0140b6c66947121555abf726574fd778225a63270cc2ccf3c39'
