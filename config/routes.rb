86ddc7fa20a4e27987cef07d82363bc571666d62
afterburnertech <brandon@afterburnertechnologies.com>
2013-02-04 12:01:35 -0500

    Mid development Ch7

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  resources :users #REST-style URI for Users Resource
 
   root to: 'static_pages#home'
 
---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+
+FactoryGirl.define do
+	factory :user do
+		name 		"Brandon Campbell"
+		email		"brandon@andon.us"
+		password	"foobar"
+		password_confirmation "foobar"
+	end
+end
