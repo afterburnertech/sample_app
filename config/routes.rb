aab992a6865741031f8a5caf76f48829b4fd2111
afterburnertech <brandon@afterburnertechnologies.com>
2013-02-04 19:38:54 -0500

    mid way sign-in-out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 SampleApp::Application.routes.draw do
   resources :users #REST-style URI for Users Resource
+  resources :sessions, only: [:new, :create, :destroy]
+
+  match '/signup',   to: 'users#new'
+  match '/signin',   to: 'sessions#new'
+  match '/signout',  to: 'sessions#destroy', via: :delete
 
   root to: 'static_pages#home'
 
